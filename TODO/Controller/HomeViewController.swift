a97b1afd30f46a10210b85025f1b4da44b1374d4
Donggaem <ehdrua465111@naver.com>
2022-06-29 02:27:44 +0900

    홈 화면 UI 재구성

--- TODO/Controller/HomeViewController.swift
+++ TODO/Controller/HomeViewController.swift
@@ -11,6 +11,8 @@ import FSCalendar
 
 class HomeViewController: UIViewController, FSCalendarDelegate, FSCalendarDataSource, FSCalendarDelegateAppearance {
 
+
+    @IBOutlet weak var year: UILabel!
     @IBOutlet weak var calendarView: FSCalendar!
     
     override func viewDidLoad() {
@@ -23,14 +25,26 @@ class HomeViewController: UIViewController, FSCalendarDelegate, FSCalendarDataSo
         calendarView.appearance.titleWeekendColor = .gray // 주말 날짜 색깔
         calendarView.appearance.weekdayTextColor = .gray // 요일 날짜 색깔
         calendarView.appearance.headerTitleColor = .black // 헤더의 폰트 색상 설정
-        
+
         calendarView.appearance.headerDateFormat = "YYYY년 MM월" // 헤더의 날짜 포맷 설정
-        calendarView.appearance.headerTitleFont = UIFont(name: "toboto", size: 18)
-        calendarView.appearance.headerTitleAlignment = .center // 헤더의 폰트 정렬 설정
+        calendarView.appearance.headerTitleAlignment = .left // 헤더의 폰트 정렬 설정
         calendarView.appearance.headerMinimumDissolvedAlpha = 0.0 // 헤더 양 옆(전달 & 다음 달) 글씨 투명도
+        calendarView.calendarHeaderView.isHidden = true
+        calendarView.headerHeight = 0
+
+        year.text = dateFormatter.string(from: calendarView.currentPage)
+        year.sizeToFit()
+        
     }
     
-
+    func calendarCurrentPageDidChange(_ calendar: FSCalendar) {
+        year.text = self.dateFormatter.string(from: calendarView.currentPage)
+        }
     
-
+    private lazy var dateFormatter: DateFormatter = {
+        let df = DateFormatter()
+        df.locale = Locale(identifier: "ko_KR")
+        df.dateFormat = "yyyy년 M월"
+        return df
+    }()
 }
