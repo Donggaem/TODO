b7f9a880f67a83a8ba179b098714fced54efe6e9
Donggaem <ehdrua465111@naver.com>
2022-07-05 23:34:25 +0900

    Home 화면 UI 조정(간격조정, 버튼 조정, 캘린더 전, 다음 달 날짜 숨기기 )

--- TODO/Controller/HomeViewController.swift
+++ TODO/Controller/HomeViewController.swift
@@ -109,6 +109,7 @@ extension HomeViewController: FSCalendarDelegate, FSCalendarDataSource, FSCalend
         calendarView.delegate = self
         calendarView.dataSource = self
 
+        calendarView.placeholderType = .none
         calendarView.appearance.titleDefaultColor = .gray // 평일 날짜 색깔
         calendarView.appearance.titleWeekendColor = .gray // 주말 날짜 색깔
         calendarView.appearance.weekdayTextColor = .gray // 요일 날짜 색깔
